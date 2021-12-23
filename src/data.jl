9c0ac2310b227877cf944757924d87fec026f127
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-12-23 17:07:14 +0900

    fix

--- src/data.jl
+++ src/data.jl
@@ -8,7 +8,7 @@ struct Profile <: AbstractData
     minbit::Float64
 end
 
-name(profile::Profile) = profile.name
+@inline name(profile::Profile) = profile.name
 @inline minbit(profile::Profile) = profile.minbit
 
 struct MSA <: AbstractData
