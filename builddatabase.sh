15a71002e0aaba6d67c93b8f523629871ef8b636
banhbio <ban@kuicr.kyoto-u.ac.jp>
2021-12-29 18:23:37 +0900

    fix path

--- builddatabase.sh
+++ builddatabase.sh
@@ -8,4 +8,4 @@ cd $WORKDIR
 module load hmmer/3.3.2
 module load julia/1.6.2
 
-julia --project=@. bin/builddatabase.jl -t 8 -i ./data/uniref/uniref90 --taxonomy_db ./data/taxonomy --seq2taxonomy_db ./data/uniref/uniref90.taxid.db --hmmdir ./data/profile --profilelist ./data/profile_list.tsv --outputdir ./data/fasta
+julia --project=@. bin/builddatabase.jl -t 8 -i ./data/uniref/uniref90 --taxonomy_db ./data/taxonomy --seq2taxonomy_db ./data/uniref/uniref90.taxid.db --hmmdir ./data/profile --profilelist ./data/profile_list.tsv --outputdir ./data/
