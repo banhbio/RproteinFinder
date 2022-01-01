22e51276bf9c01410dc43d39d4e539f6b304e4d8
banhbio <ban@kuicr.kyoto-u.ac.jp>
2022-01-01 13:06:39 +0900

    fix

--- shell/builddatabase.sh
+++ shell/builddatabase.sh
@@ -10,4 +10,4 @@ module load julia/1.6.2
 module load seqkit/2.1.0
 
 julia --project=@. bin/builddatabase.jl -t 8 -i ./data/uniref/uniref90 --taxonomy_db ./data/taxonomy --seq2taxonomy_db ./data/uniref/uniref90.taxid.db --hmmdir ./data/profile --profilelist ./data/profile_list.tsv --outputdir ./data/
-cat ./data/fasta/rproteins.tsv | cut -f1 |  seqkit grep -f - ./data/uniref/uniref90 > rproteins.fasta
+cat ./data/fasta/rproteins.tsv | cut -f1 |  seqkit grep -f - ./data/uniref/uniref90 > ./data/fasta/rproteins.fasta
